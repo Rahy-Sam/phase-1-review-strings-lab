55439c48f7a0fac39f32e05b0b06c6cd979bc653
Raymond <raymond.mwaiki@student.moringaschool.com>
2023-10-05 18:08:27 +0300

    "Done"

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const currentUser = 'Grace Hopper'
 
-const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const welcomeMessage = `Welcometo Flatbook,+${currentUser} !`;
 
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreeting = `Welcome, ${currentUser.slice(0.1)}`
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}`
